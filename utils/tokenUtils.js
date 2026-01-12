2901fe475e3dbf4f6514bd6d276388ac1239824e
OoiGuanCheng <ooiguancheng1118@1utar.my>
2026-01-12 15:22:49 +0800

    removed tokenUtils, implemented in userModel

--- utils/tokenUtils.js
+++
@@ -1,9 +0,0 @@
-const jwt = require("jsonwebtoken");
-
-const generateToken = (email) => {
-  return jwt.sign({ email: email }, process.env.JWT_SECRET, {
-    expiresIn: "1h",
-  });
-};
-
-module.exports = generateToken;
